67e55c4928163e64efec171206d63038cc9de91f
yvonnechanlove97 <46798088+yvonnechanlove97@users.noreply.github.com>
2019-04-25 16:06:36 -0500

    added to the datacleaning.R

--- MIDB_cleaning.R
+++ MIDB_cleaning.R
@@ -1,26 +1,34 @@
+
 #MIDB describes the participants in each of those disputes.
+if(!require('pacman')) install.packages('pacman')
+pacman::p_load(
+  'dplyr',
+  'tidyr',
+  'zoo',
+  'data.table'
+)
 
-#get rid of the missing, lasting days, FatalPre
 
-library(dplyr)
-library(tidyr)
-library(zoo)
 #drop dispnum4,revtype2 because they contain too many missing values
 #drop fatalpre because it contians missing values and highly correlated to fatality 
 #drop the last 4 variables which is unrelated
-data_clean=data %>% select(-c("stday","endday", 'dispnum4','revtype2',"fatalpre", "version", "changes_1", "changes_2", "changes_3"))
+MID_Actor=MID_Actor %>% select(-c('dispnum4','revtype2',"fatalpre", "version", "changes_1", "changes_2", "changes_3"))
 
-dates=data %>% select(stday:endyear)
+#record the exact dates in case
+dates=MID_Actor %>% select(stday:endyear)
+
+
+#let the missing start day and end day be the first day of the month 
+MID_Actor$stday[MID_Actor$stday==-9]=1
+MID_Actor$stday[MID_Actor$endday==-9]=1
 
 #calculate the lasting days of the dispute
-#since start and end days contain certain amount of missing value, we used the first day of each month as the measurement
-data_clean=tidyr::unite(data_clean,'startdate',c("styear","stmon"),sep='-')
-data_clean=tidyr::unite(data_clean,'enddate',c("endyear","endmon"),sep='-')
-data_clean=data_clean %>% 
-  mutate_at(c('startdate','enddate'),function(x){as.Date(as.yearmon(x))}) %>%
-  select(-c("stday","endday"))
-data_clean$last_days=data_clean$enddate-data_clean$startdate
-
+MID_Actor=tidyr::unite(MID_Actor,'startdate',c("styear","stmon","stday"),sep='-')
+MID_Actor=tidyr::unite(MID_Actor,'enddate',c("endyear","endmon","endday"),sep='-')
+MID_Actor=MID_Actor %>% 
+  mutate_at(c('startdate','enddate'),function(x){as.Date(x)}) %>%
+  mutate(last_days=enddate-startdate)
+MID_Actor[MID_Actor$last_days==0]=1
 
 #does hostlev has anything to do with fatality level
-#probably change to only use the first day of a month for missing values on start and end days                     
+fwrite(data_clean,file="MIDB_4.2_Cleaned.csv")
